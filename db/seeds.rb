eae5e02dfd2f39a1041240abcbb5dcbf6a76fa1d
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-08-11 16:52:44 +0900

    added show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+12.times do
+  Flat.create(
+    name: Faker::Dessert.topping,
+    address: Faker::Address.street_address,
+    description: Faker::Marketing.buzzwords,
+    price_per_night: [75, 100, 200, 8000, 10].sample,
+    number_of_guests: [1, 2, 3].sample
+  )
+end
